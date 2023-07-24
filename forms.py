f2ef6ddce04f11f787e67fcdfff60e6ef2ff602d
Ivan Khamer <hamerivan895@gmail.com>
2023-07-24 17:36:40 +0300

    Initial commit

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import InputRequired
-
-
-class CitiesForm(FlaskForm):
-    city_1 = StringField(validators=[InputRequired()])
-    city_2 = StringField(validators=[InputRequired()])
-
